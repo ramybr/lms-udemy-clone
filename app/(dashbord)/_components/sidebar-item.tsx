3b8cdea1077bd372b678d10d14718f418c243d28
Ramy Ben Rejeb <ramybr2903@gmail.com>
2024-02-28 17:46:14 +0100

    navbar + mobile sidebar

--- app/(dashbord)/_components/sidebar-item.tsx
+++ app/(dashbord)/_components/sidebar-item.tsx
@@ -40,6 +40,12 @@ export const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
         />
         {label}
       </div>
+      <div
+        className={cn(
+          "ml-auto opacity-0 border-2 border-sky-700 h-full transition-all",
+          isActive && "opacity-100"
+        )}
+      />
     </button>
   );
 };
