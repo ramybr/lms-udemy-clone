c238c5b4f9d0870bd719c324ca63d5bbcfab31b4
Ramy Ben Rejeb <ramybr2903@gmail.com>
2024-03-06 16:02:26 +0100

    course category form

---
+++ scripts/seed.ts
@@ -0,0 +1,27 @@
+const { PrismaClient } = require("@prisma/client");
+
+const database = new PrismaClient();
+
+async function main() {
+  try {
+    await database.category.createMany({
+      data: [
+        { name: "Computer Science" },
+        { name: "Music" },
+        { name: "Fitness" },
+        { name: "Photography" },
+        { name: "Accounting" },
+        { name: "Engeneering" },
+        { name: "Video Editing" },
+      ],
+    });
+
+    console.log("sucess");
+  } catch (error) {
+    console.log("Error seeding the database categories", error);
+  } finally {
+    await database.$disconnect();
+  }
+}
+
+main();
