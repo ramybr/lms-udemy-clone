e64ac3eb6cb3b482aca1e6eaef97c22124fff043
Ramy Ben Rejeb <ramybr2903@gmail.com>
2024-03-11 12:09:57 +0100

    chapter video form

---
+++ app/(dashbord)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+
+import { Button } from "@/components/ui/button";
+import { Pencil, PlusCircle, VideoIcon } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { Chapter, MuxData } from "@prisma/client";
+import { FileUpload } from "@/components/file-upload";
+
+type ChapterVideoFormProps = {
+  initialData: Chapter & { muxData?: MuxData | null };
+  courseId: string;
+  chapterId: string;
+};
+
+const formSchema = z.object({
+  videoUrl: z.string().min(1),
+});
+
+export const ChapterVideoForm = ({
+  initialData,
+  courseId,
+  chapterId,
+}: ChapterVideoFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEdit = () => setIsEditing((current) => !current);
+
+  const router = useRouter();
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(
+        `/api/courses/${courseId}/chapter/${chapterId}`,
+        values
+      );
+      toast.success("Course edited");
+      toggleEdit();
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong");
+    }
+  };
+
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Chapter video
+        <Button onClick={toggleEdit} variant="ghost">
+          {isEditing && <>Cancel</>}
+          {!isEditing && !initialData.videoUrl && (
+            <>
+              <PlusCircle className="h-4 w-4 mr-2" />
+              Add video
+            </>
+          )}
+          {!isEditing && initialData.videoUrl && (
+            <>
+              <Pencil className="h-4 m-4 mr-2" />
+              Edit video
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing &&
+        (!initialData.videoUrl ? (
+          <div className="flex items-center justify-center h-60 bg-slate-200 rounded-md">
+            <VideoIcon className="h-10 w-10 text-slate-500" />
+          </div>
+        ) : (
+          <div className="relative aspect-video mt-2">Video uploaded!</div>
+        ))}
+      {isEditing && (
+        <div>
+          <FileUpload
+            endpoint="chapterVideo"
+            onChange={(url) => {
+              if (url) {
+                onSubmit({ videoUrl: url });
+              }
+            }}
+          />
+          <div className="text-xs text-muted-foreground mt-4">Upload video</div>
+        </div>
+      )}
+      {initialData.videoUrl && !isEditing && (
+        <div className="text-xs text-muted-foreground mt-2">
+          This can take few minutes. Refrech the page if the video does not
+          appear.
+        </div>
+      )}
+    </div>
+  );
+};
