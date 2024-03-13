79ada5b67bf68b884bde996d0db935dc61913273
Ramy Ben Rejeb <ramybr2903@gmail.com>
2024-03-13 16:25:55 +0100

    course grid & card UI 2

--- app/(dashbord)/(routes)/search/page.tsx
+++ app/(dashbord)/(routes)/search/page.tsx
@@ -1,12 +1,33 @@
 import { db } from "@/lib/db";
 import { Categories } from "./_components/categories";
 import { SearchInput } from "@/components/search-input";
+import { getCourses } from "@/actions/get-courses";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const SearchPage = async () => {
+type SearchPageProps = {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+};
+
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
   const categories = await db.category.findMany({
     orderBy: {
       name: "asc",
     },
+  });
+
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
   });
 
   return (
