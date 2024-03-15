871c4bacd2021a2b7757548c31caf5e089d397f0
Ramy Ben Rejeb <ramybr2903@gmail.com>
2024-03-15 23:40:43 +0100

    chapter progress functinnality 2

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-progress-button.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useConfettiStore } from "@/hooks/use-confetti-store";
+import axios from "axios";
+import Icon, { CheckCircle, XCircle } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+type CourseProgressButtonProps = {
+  chapterId: string;
+  courseId: string;
+  nextChapterId: string;
+  isCompleted: boolean;
+};
+
+export const CourseProgressButton = ({
+  chapterId,
+  courseId,
+  nextChapterId,
+  isCompleted,
+}: CourseProgressButtonProps) => {
+  const router = useRouter();
+  const confetti = useConfettiStore();
+  const [isLoading, setIsLoding] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoding(true);
+
+      await axios.put(
+        `/api/courses/${courseId}/chapters/${chapterId}/progress`,
+        {
+          isCompleted: !isCompleted,
+        }
+      );
+      if (!isCompleted && !nextChapterId) {
+        confetti.onOpen();
+      }
+
+      if (!isCompleted && nextChapterId) {
+        router.push(`/courses/${courseId}/chapters/${nextChapterId}`);
+      }
+
+      toast.success("Progress updated");
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoding(false);
+    }
+  };
+  const Icon = isCompleted ? XCircle : CheckCircle;
+
+  return (
+    <Button
+      onClick={onClick}
+      disabled={isLoading}
+      type="button"
+      variant={isCompleted ? "outline" : "success"}
+      className="w-full md:w-auto"
+    >
+      {isCompleted ? "Not completed" : "Mark as complete"}
+      <Icon className="h-4 w-4 ml-2" />
+    </Button>
+  );
+};
--- components/course-card.tsx
+++ components/course-card.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { IconBadge } from "@/components/icon-badge";
 import { BookOpen } from "lucide-react";
 import { formatPrice } from "@/lib/format";
+import { CourseProgress } from "@/components/course-progress";
 
 type CourseCardProps = {
   key: string;
@@ -43,8 +44,12 @@ export const CourseCard = ({
               </span>
             </div>
           </div>
-          {progress != null ? (
-            <div>TODO: progress component</div>
+          {progress === null ? (
+            <CourseProgress
+              variant={progress === 100 ? "success" : "default"}
+              size="sm"
+              value={progress!}
+            />
           ) : (
             <p className=" text-md md:text-sm font-medium text-slate-700">
               {formatPrice(price)}
